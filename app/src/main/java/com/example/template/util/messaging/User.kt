0227cc4ea82e9ad47ce891bd60544de20907832d
storeIt <syncredex@gmail.com>
2020-12-30 15:18:34 +0200

    remove redundant code

--- app/src/main/java/com/example/template/util/messaging/User.kt
+++ app/src/main/java/com/example/template/util/messaging/User.kt
@@ -2,30 +2,4 @@ package com.example.template.util.messaging
 
 class User (
         var name: String
-) : MessageMenager<String>() {
-
-    private val usersWithEmails: MutableList<User> = mutableListOf()
-
-    constructor(
-            name: String,
-            email: String
-    ) : this(name) {
-        usersWithEmails.add(User(name, email))
-    }
-
-    constructor(
-            name: String,
-            email: String,
-            event: Event
-    ) : this(name, email){
-
-    }
-
-    private fun setMessageReceiver(user: User, event: Event){
-
-        
-
-    }
-
-
-}
+)
